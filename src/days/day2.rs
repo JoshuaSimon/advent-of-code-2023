e26d32df333e356edd54c8f6e0d0071e51543421
Joshua Simon <joshsimon96@googlemail.com>
2023-12-03 15:16:54 +0100

    Day 2 part 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -37,7 +37,6 @@ pub fn solve_part1(input_lines: impl Iterator<Item = io::Result<String>>) {
                         // Get avilable count of the color
                         let count_avilable: &u32 = cube_counts.get(cube_color).unwrap();
                         if cube_count > *count_avilable && game_possible {
-                            println!("ID: {}", game_id);
                             game_possible = false;
                             sum += game_id;
                             break;
@@ -50,21 +49,52 @@ pub fn solve_part1(input_lines: impl Iterator<Item = io::Result<String>>) {
             }
         }
     }
-    println!("Result of day 1 part 1 is: {}", game_sum - sum);
+    println!("Result of day 2 part 1 is: {}", game_sum - sum);
 }
 
 /// Parses the input of day 1 part 2 and solves the puzzle.
 pub fn solve_part2(input_lines: impl Iterator<Item = io::Result<String>>) {
-    
+    let mut sum: u32 = 0;
     for line in input_lines {
         match line {
             Ok(content) => {
-                
+                // Container for maximal cube counts of a game
+                let mut cube_counts: HashMap<&str, u32> = HashMap::from([
+                    ("red", 0),
+                    ("green", 0),
+                    ("blue", 0),
+                ]);
+
+                // Get the cube counts
+                let split_result: Vec<&str> = content.split(":").collect();
+                let cube_string: Vec<&str> = split_result.last().unwrap().split(";").collect();
+                for round in cube_string {
+                    let draws:  Vec<&str> = round.split(",").collect();
+                    for draw in draws {
+                        let draw = draw.trim();
+                        let draw_split: Vec<&str> = draw.split(" ").collect();
+                        let cube_count: u32 = draw_split.first().unwrap().parse::<u32>().unwrap();
+                        let cube_color: &str = draw_split.last().unwrap();
+
+                        let max_cube_conut: &u32 = cube_counts.get(cube_color).unwrap();
+                        if cube_count > *max_cube_conut {
+                            cube_counts.insert(cube_color, cube_count);
+                        }
+                    }
+                }
+
+                // Calculate the power of the game
+                let mut power: u32 = 1;
+                for val in cube_counts.values() {
+                    power *= val;
+                }
+
+                sum += power;
             }
             Err(err) => {
                 eprintln!("Error reading line: {}", err);
             }
         }
     }
-    println!("Result of day 1 part 2 is: {}", "");
+    println!("Result of day 2 part 2 is: {}", sum);
 }
